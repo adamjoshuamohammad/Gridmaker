0c9cdf9e244d2ea636fe7a141c060c55c4e6dbb2
Adam Joshua <adamjoshuamohammad@gmail.com>
2021-10-21 14:52:37 -0400

    implemented coloring functions

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ let colorSelected;
 
 //Add a row
 function addR() {
-    alert("Clicked Add Row")
+    //alert("Clicked Add Row")
     let newRow = grid.appendChild(document.createElement("tr"));
     let newCell = newRow.appendChild(document.createElement("td"));
     if (numRows === 0) {
         numCols = 0;   
-        newCell.onclick = getCellColor;
+        newCell.onclick = paintCell;
         numRows++;
         numCols++;
     }
     else {
-        newCell.onclick = getCellColor;
+        newCell.onclick = paintCell;
         for (let i = 1; i < numCols; i++) {
-            newRow.appendChild(document.createElement("td")).onclick = getCellColor;
+            newRow.appendChild(document.createElement("td")).onclick = paintCell;
         }
         numRows++;
     }
@@ -24,16 +24,16 @@ function addR() {
 }
 //Add a column
 function addC() {
-    alert("Clicked Add Col")
+    //alert("Clicked Add Col")
     if (numCols === 0) {
         numRows = 0;
-        grid.appendChild(document.createElement("tr")).appendChild(document.createElement("td")).onclick = getCellColor;
+        grid.appendChild(document.createElement("tr")).appendChild(document.createElement("td")).onclick = paintCell;
         numRows++;
         numCols++;
     }
     else {  
         for (let i = 0; i < numRows; i++) {
-            grid.rows[i].appendChild(document.createElement("td")).onclick = getCellColor;
+            grid.rows[i].appendChild(document.createElement("td")).onclick = paintCell;
         }
         numCols++; 
     } 
@@ -42,7 +42,7 @@ function addC() {
 
 //Remove a row
 function removeR() {
-    alert("Clicked Remove Row")
+    //alert("Clicked Remove Row")
     if (numRows !== 0) 
     {
         grid.removeChild(grid.lastElementChild);
@@ -57,7 +57,7 @@ function removeR() {
 }
 //Remove a column
 function removeC() {
-    alert("Clicked Remove Col")
+    //alert("Clicked Remove Col")
     if (numCols !== 0) 
     {
         for (let i = 0; i < grid.rows.length; i++) {
@@ -80,18 +80,35 @@ function selected(){
     console.log(colorSelected);
 }
 
-function fill(){
-    alert("Clicked Fill All")
+function fill() {
+    //alert("Clicked Fill All")
+    paintGrid(colorSelected);
 }
 
 function clearAll(){
-    alert("Clicked Clear All")
+    //alert("Clicked Clear All")
+    paintGrid("white");
 }
 
 function fillU(){
-    alert("Clicked Fill All Uncolored")
+    //alert("Clicked Fill All Uncolored")
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+            if (grid.querySelectorAll("tr")[i].querySelectorAll("td")[j].style.backgroundColor === "" || grid.querySelectorAll("tr")[i].querySelectorAll("td")[j].style.backgroundColor === "white") {
+                grid.querySelectorAll("tr")[i].querySelectorAll("td")[j].style.backgroundColor = colorSelected;
+            }
+        }
+    }
 }
 
-function getCellColor() { this.style.backgroundColor = colorSelected; } 
+function paintCell() { this.style.backgroundColor = colorSelected; } 
+
+function paintGrid(color) {
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+            grid.querySelectorAll("tr")[i].querySelectorAll("td")[j].style.backgroundColor = color;
+        }
+    }
+}
 
 function printGridSize() { console.log(`columns: ${numCols} rows:  ${numRows}`); }
